fd806253833ff49068f1c42343154283bbc83a2f
JIWONKIM <4526as@naver.com>
2024-10-10 12:50:01 +0900

    exitUpdate

--- utils/io.js
+++ utils/io.js
@@ -1,25 +1,26 @@
-// 통신 관련 파일
 const chatController = require("../Controllers/chat.controller.js");
 const userController = require("../Controllers/user.controller.js");
 
 module.exports = function(io) {
-    let connectedUsers = 0; // 현재 연결된 사용자 수
+    let connectedUsers = 0;
+    const users = {}; // 사용자 정보를 저장할 객체 추가
 
     io.on("connection", async (socket) => {
-        connectedUsers++; // 사용자가 연결될 때 사용자 수 증가
-        io.emit("userCount", connectedUsers); // 현재 사용자 수를 모든 클라이언트에 전송
+        connectedUsers++;
+        io.emit("userCount", connectedUsers);
         console.log("client is connected", socket.id);
 
         socket.on("login", async (userName, cb) => {
             console.log("User name received:", userName);
             try {
                 const user = await userController.saveUser(userName, socket.id);
+                users[socket.id] = user; // 소켓 ID를 키로 사용자 정보를 저장
                 cb({ ok: true, data: user });
                 const welcomeMessage = {
                     chat: `${user.name} 님이 들어왔습니다.`,
                     user: { id: null, name: "system" },
                 };
-                io.emit("message", welcomeMessage); // 모든 클라이언트에 메시지 전송
+                io.emit("message", welcomeMessage);
             } catch (error) {
                 cb({ ok: false, error: error.message });
             }
@@ -29,26 +30,40 @@ module.exports = function(io) {
             try {
                 const user = await userController.checkUser(socket.id);
                 const newMessage = await chatController.saveChat(message, user);
-                io.emit("message", newMessage); // 모든 클라이언트에 새 메시지 전송
+                io.emit("message", newMessage);
                 cb({ ok: true });
             } catch (error) {
-                cb({ ok: false, error: error.message });
+                cb({ ok: false, error: "메시지 전송 실패: " + error.message });
             }
         });
 
-        // 연결 오류 로그 추가
-        socket.on("error", (error) => {
-            console.error("Socket error:", error);
+        socket.on("userLeave", async (userName, cb) => {
+            connectedUsers--;
+            const leaveMessage = {
+                chat: `${userName} 님이 나갔습니다.`,
+                user: { id: null, name: "system" },
+            };
+            io.emit("message", leaveMessage);
+            io.emit("userCount", connectedUsers);
+            cb({ ok: true });
         });
 
         socket.on("disconnect", () => {
-            connectedUsers--; // 사용자가 연결 해제될 때 사용자 수 감소
-            io.emit("userCount", connectedUsers); // 현재 사용자 수를 모든 클라이언트에 전송
+            const user = users[socket.id]; // 연결이 끊어진 사용자를 찾음
+            if (user) {
+                connectedUsers--;
+                const leaveMessage = {
+                    chat: `${user.name} 님이 나갔습니다.`,
+                    user: { id: null, name: "system" },
+                };
+                io.emit("message", leaveMessage);
+                io.emit("userCount", connectedUsers);
+                delete users[socket.id]; // 사용자 정보 삭제
+            }
             console.log("client disconnected", socket.id);
         });
     });
 
-    // 서버에 연결 실패 시 오류 처리
     io.on("error", (error) => {
         console.error("Server error:", error);
     });
