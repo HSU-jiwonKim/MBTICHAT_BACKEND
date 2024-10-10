0e0dfa8fd1b714f7e6780972b44ec4249f15de19
JIWONKIM <4526as@naver.com>
2024-10-10 22:46:05 +0900

    lastlastlast commit

--- utils/io.js
+++ utils/io.js
@@ -3,17 +3,15 @@ const userController = require("../Controllers/user.controller.js");
 
 module.exports = function(io) {
     let connectedUsers = 0;
-    const users = {}; // 사용자 정보를 저장할 객체 추가
+    const users = {}; // 사용자 정보를 저장할 객체
 
     io.on("connection", async (socket) => {
-        // 이미 연결된 사용자가 있는 경우 처리
+        // 소켓 ID로 기존 사용자 확인
         if (users[socket.id]) {
             console.log("기존 사용자 재연결:", socket.id);
             return; // 기존 사용자일 경우 새로운 연결을 만들지 않음
         }
 
-        connectedUsers++;
-        io.emit("userCount", connectedUsers);
         console.log("client is connected", socket.id);
 
         socket.on("login", async (userName, cb) => {
@@ -29,12 +27,16 @@ module.exports = function(io) {
                     cb({ ok: false, error: "이미 사용 중인 닉네임입니다." });
                     return;
                 }
-                
+
+                // 사용자 정보를 저장
                 const user = await userController.saveUser(userName, socket.id);
                 users[socket.id] = user; // 소켓 ID를 키로 사용자 정보를 저장
+                connectedUsers++; // 새로운 사용자가 연결되었으므로 증가
+                io.emit("userCount", connectedUsers); // 사용자 수 업데이트
+
                 cb({ ok: true, data: user });
-                
-                // 사용자에게 그날의 날짜 메시지 보내기
+
+                // 날짜 메시지 전송
                 const today = new Date();
                 const options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', locale: 'ko-KR' };
                 const dateMessage = {
@@ -75,20 +77,23 @@ module.exports = function(io) {
                 console.error("Callback is not a function");
                 return;
             }
-            connectedUsers--;
-            const leaveMessage = {
-                chat: `${userName} 님이 나갔습니다.`,
-                user: { id: null, name: "system" },
-            };
-            io.emit("message", leaveMessage);
-            io.emit("userCount", connectedUsers);
+            if (users[socket.id]) { // 사용자 정보가 있을 경우에만 감소
+                connectedUsers--;
+                const leaveMessage = {
+                    chat: `${userName} 님이 나갔습니다.`,
+                    user: { id: null, name: "system" },
+                };
+                io.emit("message", leaveMessage);
+                io.emit("userCount", connectedUsers);
+                delete users[socket.id]; // 사용자 정보 삭제
+            }
             cb({ ok: true });
         });
 
         socket.on("disconnect", () => {
             const user = users[socket.id]; // 연결이 끊어진 사용자를 찾음
             if (user) {
-                connectedUsers--;
+                connectedUsers--; // 연결된 사용자 수 감소
                 const leaveMessage = {
                     chat: `${user.name} 님이 나갔습니다.`,
                     user: { id: null, name: "system" },
@@ -110,7 +115,6 @@ module.exports = function(io) {
                 chat: `${now.toLocaleDateString('ko-KR', options)}입니다.`,
                 user: { id: null, name: "system" },
             };
-            // 연결된 모든 소켓에게 발송
             io.sockets.emit("message", dateMessage);
         }
     }, 60000); // 1분마다 체크
