1e40347012fa7543c51e3fc31293ee950c693d98
JIWONKIM <4526as@naver.com>
2024-10-26 10:44:44 +0900

    gemini

--- utils/io.js
+++ utils/io.js
@@ -1,9 +1,9 @@
 import { Server } from 'socket.io';
 import { GoogleAuth } from 'google-auth-library';
+import { v2beta3 } from '@google-cloud/aiplatform';
 import dotenv from 'dotenv';
 import chatController from '../Controllers/chat.controller.js';
 import userController from '../Controllers/user.controller.js';
-import { v2beta3 } from '@google-cloud/aiplatform'; // ES 모듈 모드로 수정
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ const clientOptions = {
     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   },
 };
-const predictionServiceClient = new v2beta3.PredictionServiceClient(clientOptions); // predictionServiceClient 선언
+const predictionServiceClient = new v2beta3.PredictionServiceClient(clientOptions);
 
 // API 호출 쿨다운 설정
 let lastGPTCallTime = 0;
@@ -108,8 +108,7 @@ export default function (io) {
             instances: [{ content: prompt }],
             parameters,
           };
-
-          const [response] = await predictionServiceClient.predict(request); // predictionServiceClient 사용
+          const [response] = await predictionServiceClient.predict(request);
           const geminiMessage = response.predictions[0].text; // 응답에서 텍스트 추출
 
           const botMessage = {
