47e9b8768864cb59516a8149f973385d80c3c996
JIWONKIM <4526as@naver.com>
2024-10-28 01:04:38 +0900

    login3

--- Controllers/chat.controller.js
+++ Controllers/chat.controller.js
@@ -1,4 +1,4 @@
-import Chat from '../Models/chat.js'; // import로 Chat 모델 가져오기
+import Chat from '../Models/chat.js';
 
 const chatController = {};
 
@@ -7,13 +7,13 @@ chatController.saveChat = async (message, user) => {
         chat: message,
         user: {
             id: user._id,
-            name: user.name,
+            name: user.nickname, // user.name 대신 user.nickname 사용
         },
-        timestamp: new Date(), // 현재 시간을 추가
+        timestamp: new Date(), 
     });
 
-    await newMessage.save(); // 메시지 저장
+    await newMessage.save(); 
     return newMessage;
 };
 
-export default chatController; // chatController 내보내기
+export default chatController;
