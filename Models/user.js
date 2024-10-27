fb58fd5593a704acb934aad6af909ca7fea2803c
JIWONKIM <4526as@naver.com>
2024-10-27 10:17:10 +0900

    login

--- Models/user.js
+++ Models/user.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
         required: [true, "User must type name"], // 필수 입력 사항이며, 오류 메시지 설정
         unique: true, // 유일한 값이어야 함
     },
+    password: { // 유저의 비밀번호를 저장하는 필드
+        type: String, // 데이터 타입: 문자열
+        required: [true, "Password is required"], // 필수 입력 사항
+    },
     token: { // 유저의 연결 정보(id)를 저장하는 필드
         type: String, // 데이터 타입: 문자열
     },
